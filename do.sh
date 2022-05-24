8b3b5f965dba444d51ad3d0e3465df0e2b1af5a5
xsser <xsser@users.noreply.github.com>
2022-05-24 11:16:15 +0800

    Add files via upload

---
+++ do.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#do.sh 17557282005 740d926b981716f472f19b11ccb00699eac6a08b371b0c3a3fe2c655005bc47c
+auth=$1
+telephone=$0
+
+cp ./nb /root/nb && mv /root/nb /root/sams \
+echo "29 11 * * * cd /root && nohup ./sams -telephone $telephone -authToken $auth  -payMethod 1  -deliveryType 2 -addressId 0 >/tmp/log.log 2>&1 & " >> /etc/crontab \
+echo "59 13 * * * cd /root && nohup ./sams -telephone $telephone -authToken $auth  -payMethod 1  -deliveryType 2 -addressId 0 >/tmp/log.log 2>&1 & " >> /etc/crontab \
+echo "35 11 * * * killall sams" >> /etc/crontab \
+echo "05 13 * * * killall sams" >> /etc/crontab \
+systemctl restart crond
+echo "部署成功!"
+
